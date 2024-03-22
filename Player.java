02c3d73aebf3a08db20a7e5f9f8fb06de6d756a7
1109256 <1109256@NCHS120490486.ms.nsd.org>
2024-03-22 09:57:57 -0700

    Rotation works, based on non limited x and y velocity.

--- Player.java
+++ Player.java
@@ -13,11 +13,17 @@ public class Player {
 	private int height;
 	private int moveSpeedMod = 12;
 	private int[][] pastMovement = new int[20][2];
-	private double xDir = 0;
-	private double yDir = 0;
+	private double xVelocity = 0;
+	private double yVelocity = 0;
+	private double trueXVelocity = 0;
+	private double trueYVelocity = 0;
 	private Image playerImage;
+	private int drawWidth;
+	private int drawHeight;
+	//Direction the player icon should be facing in, measured in radians
+	private double facingDir;
 
-	public Player(int x, int y, int width, int height, int moveSpeedMod) {
+	public Player(int x, int y, int width, int height, int moveSpeedMod, int drawWidth, int drawHeight) {
 		this.x = x;
 		this.y = y;
 		this.width = width;
@@ -28,6 +34,8 @@ public class Player {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+		this.drawWidth = drawWidth;
+		this.drawHeight = drawHeight;
 	}
 
 	public Player() {
@@ -36,6 +44,8 @@ public class Player {
 		this.width = 400;
 		this.height = 400;
 		this.moveSpeedMod = 60;
+		this.drawWidth = 100;
+		this.drawHeight = 100;
 		try {
 			playerImage = ImageIO.read(new File("Hyena-S2.png")); // Replace "player.png" with the path to your image file
 		} catch (IOException e) {
@@ -43,22 +53,24 @@ public class Player {
 		}
 	}
 
-	public void move(double xDir, double yDir) {
-		this.xDir += xDir / moveSpeedMod;
-		this.yDir += yDir / moveSpeedMod;
-		this.x += (int) this.xDir;
-		this.y += (int) this.yDir;
-		this.xDir *= 5.0 / 6;
-		this.yDir *= 5.0 / 6;
-		if (this.xDir > 6) {
-			this.xDir = 6;
-		} else if (this.xDir < -6) {
-			this.xDir = -6;
+	public void move(double x, double y) {
+		this.xVelocity += x / this.moveSpeedMod;
+		this.yVelocity += y / this.moveSpeedMod;
+		this.x += (int) this.xVelocity;
+		this.y += (int) this.yVelocity;
+		this.xVelocity *= 5.0 / 6;
+		this.yVelocity *= 5.0 / 6;
+		this.trueXVelocity = this.xVelocity;
+		this.trueYVelocity = this.yVelocity;
+		if (this.xVelocity > 6) {
+			this.xVelocity = 6;
+		} else if (this.xVelocity < -6) {
+			this.xVelocity = -6;
 		}
-		if (this.yDir > 6) {
-			this.yDir = 6;
-		} else if (this.yDir < -6) {
-			this.yDir = -6;
+		if (this.yVelocity > 6) {
+			this.yVelocity = 6;
+		} else if (this.yVelocity < -6) {
+			this.yVelocity = -6;
 		}
 	}
 
@@ -69,11 +81,20 @@ public class Player {
 	public void draw(Graphics g, int cursorX, int cursorY) {
 	    Graphics2D g2d = (Graphics2D) g;
 	    AffineTransform old = g2d.getTransform();
-	    
-	    double angle = Math.atan2(cursorY - width / 2, height - 200);
-	    System.out.println(cursorX + "    " + cursorY);
-	    g2d.rotate(angle, width / 2, height / 2);
-	    g2d.drawImage(playerImage, 200 - playerImage.getWidth(null) / 2, 200 - playerImage.getHeight(null) / 2, null);
+	    System.out.println(xVelocity + "   " );
+	    double angle = 0;
+	    cursorX -= this.width / 2;
+	    cursorY -= this.height / 2;
+	    cursorY *= -1;
+	    double vect_len = Math.sqrt(Math.pow(this.trueXVelocity, 2) + Math.pow(this.trueYVelocity, 2));
+	    if (trueYVelocity * -1 < 0) {
+	    	angle = Math.PI * 2 - Math.acos(this.trueXVelocity / vect_len);
+	    } else {
+	    	angle = Math.acos(trueXVelocity / vect_len);
+	    }
+	    this.facingDir = 3 * Math.PI / 2 - angle;
+	    g2d.rotate(this.facingDir, this.width / 2, this.height / 2);
+	    g2d.drawImage(this.playerImage, this.width / 2 - this.drawWidth / 2, this.width / 2 - this.drawHeight / 2, this.drawWidth, this.drawHeight, null);
 	    
 	    g2d.setTransform(old);
 	}
