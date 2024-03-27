1f842862243a665367eeaf4adfcf68d724d6fb0f
1109256 <1109256@NCHS120490483.ms.nsd.org>
2024-03-27 09:34:19 -0700

    do tihs

--- Bot.java
+++ Bot.java
@@ -4,7 +4,7 @@ import java.awt.geom.AffineTransform;
 
 public class Bot extends Entity {
 	public Bot() {
-		this(0.0, 0.0, 400, 400, 60, "Hyena-S2.png", 100, 100);
+		this(0.0, 0.0, 400, 400, 120, "Hyena-S2.png", 100, 100);
 	}
 
 	public Bot(double x, double y, int width, int height, int moveSpeedMod, String imgPath, int drawWidth,
@@ -14,11 +14,23 @@ public class Bot extends Entity {
 	}
 
 	@Override
-	public void move(double x, double y) {
-
+	public void move(double playerX, double playerY) {
+		// TODO do this
 	}
 	@Override
-	public void draw(Graphics g, int cursorX, int cursorY) {
-	 
+	public void draw(Graphics g, int playerX, int playerY) {
+		Graphics2D g2d = (Graphics2D) g;
+//	    AffineTransform old = g2d.getTransform();
+//	    double angle = 0;
+//	    double vect_len = Math.sqrt(Math.pow(this.getxVelocity(), 2) + Math.pow(this.getyVelocity(), 2));
+//	    if (getyVelocity() * -1 < 0) {
+//	    	angle = Math.PI * 2 - Math.acos(this.getxVelocity() / vect_len);
+//	    } else {
+//	    	angle = Math.acos(getxVelocity() / vect_len);
+//	    }
+//	    this.setFacingDir(3 * Math.PI / 2 - angle);
+//	    g2d.rotate(this.getFacingDir(), this.getWidth() / 2, this.getHeight() / 2);
+	    g2d.drawImage(this.getPlayerImage(),(int) (this.getX() - playerX - this.getDrawWidth()),(int) (this.getY() - playerY - this.getDrawHeight()), this.getDrawWidth(), this.getDrawHeight(), null);
+//	    g2d.setTransform(old);
 	}
 }
